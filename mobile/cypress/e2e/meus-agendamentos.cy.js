a50d735bfb125efea0ae761bc9d780296180f57c
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-01-22 08:59:45 -0300

    test completed

--- mobile/cypress/e2e/meus-agendamentos.cy.js
+++ mobile/cypress/e2e/meus-agendamentos.cy.js
@@ -34,7 +34,30 @@ describe('Meus agendamentos', () => {
                 })
             })
 
+            cy.viewport('iphone-xr')
+            cy.visit('/')
 
+            cy.contains('p', 'Faça login com a sua conta')
+                .should('be.visible')
+
+            cy.login(profissional)
+            cy.verificaUsuarioLogado(profissional)
+
+            cy.get('ul li')
+                .should('be.visible')
+                .and('have.length', agendamentos.length)
+                .each(($li, index) => {
+
+                    cy.log(JSON.stringify(agendamentos[index]))
+
+                    const agendamento = agendamentos[index]
+                    const resultado = `${agendamento.servico.descricao} no dia ${agendamento.data} às ${agendamento.hora}`
+
+                    cy.wrap($li)
+                        .invoke('text')
+                        .should('contain', agendamento.usuario.nome)
+                        .and('contain', resultado)
+                })
         })
     })
 })
