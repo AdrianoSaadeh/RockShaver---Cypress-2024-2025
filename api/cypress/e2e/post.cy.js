0d47b0b7806ea261f2a6b95f9596b9ac0ae62c7f
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-02-04 08:18:49 -0300

    tests added

--- api/cypress/e2e/post.cy.js
+++ api/cypress/e2e/post.cy.js
@@ -1,11 +1,6 @@
 describe('POST /api/agendamentos', () => {
   it('Deve criar um novo agendamento', () => {
 
-    cy.deleteMany(
-      { matricula: '1001' },
-      { collection: 'agendamentos' }
-    )
-
     const body = {
       "emailCliente": "gruma.legal@gmail.com",
       "nomeCliente": "Gruma Saadeh",
@@ -15,17 +10,54 @@ describe('POST /api/agendamentos', () => {
       "codigoServico": "1"
     }
 
-    cy.api({
-      method: 'POST',
-      url: '/api/agendamentos',
-      headers: {
-        'Authorization': 'Bearer 3a8a9b8fae87baf503e7c5fe5b97fd72'
-      },
-      body: body
-    }).should((response) => {
+    cy.deleteMany(
+      { matricula: body.matricula },
+      { collection: 'agendamentos' }
+    )
+
+    cy.postAgendamento(body).should((response) => {
       expect(response.status).to.eq(201)
       expect(response.body.message).to.eq('Agendamento criado com sucesso')
       expect(response.body.agendamentoId).to.match(/^[a-fA-F0-9]{24}$/)
     })
+  })
+
+
+  it('Deve retornar erro quando o agendamento ja existe', () => {
+    const body = {
+      "emailCliente": "aline.legal@gmail.com",
+      "nomeCliente": "Aline Santos",
+      "data": "20/12/2024",
+      "hora": "16:00",
+      "matricula": "1002",
+      "codigoServico": "2"
+    }
+
+    cy.deleteMany(
+      { matricula: body.matricula },
+      { collection: 'agendamentos' }
+    )
+
+    cy.postAgendamento(body).should((response) => {
+      expect(response.status).to.eq(201)
+      expect(response.body.message).to.eq('Agendamento criado com sucesso')
+    })
+
+    cy.postAgendamento(body).should((response) => {
+      expect(response.status).to.eq(409)
+      expect(response.body.message).to.eq('Já existe um agendamento para esta data e hora. Por favor, escolha outro horário.')
+    })
+  })
+})
+
+Cypress.Commands.add('postAgendamento', (body) => {
+  return cy.api({
+    method: 'POST',
+    url: '/api/agendamentos',
+    headers: {
+      'Authorization': 'Bearer 3a8a9b8fae87baf503e7c5fe5b97fd72'
+    },
+    body: body,
+    failOnStatusCode: false
   })
 })
